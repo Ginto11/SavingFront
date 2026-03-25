c9bb0a5cb82b1024723316167ed377e89242f074
Ginto11 <salinitosnelson@gmail.com>
2026-03-25 12:21:52 -0500

    Agregando modulo de trabajo de aplicaciones

---
+++ src/app/pages/trabajo-aplicaciones/trabajo-aplicaciones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trabajo-aplicaciones',
+  imports: [],
+  templateUrl: './trabajo-aplicaciones.component.html',
+  styles: ``
+})
+export default class TrabajoAplicacionesComponent {
+
+}
